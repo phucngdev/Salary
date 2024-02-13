c18b11a1f8629f628d5b5c1a0ae8b97df9c5a18e
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-02-13 17:40:09 +0700

    new update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         <img
           src={banner}
           alt="banner"
-          className="fixed top-0 left-0 bottom-0 right-0 z-[99] w-full h-full object-cover"
+          className="fixed top-0 left-0 bottom-0 right-0 z-[999] w-full h-full object-cover"
         />
       </CSSTransition>
       <Header></Header>
